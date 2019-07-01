1826a34dc51dd8a5b17e19fcbb7f9142ab2415f8
Peter Huizenga <peterhuizenga@lclark.edu>
2019-07-01 13:49:00 -0700

    Sorry was on Liza branch, but this adds remove/trade functionality to dialogue

--- Assets/Dialogue/DialogueManager.cs
+++ Assets/Dialogue/DialogueManager.cs
@@ -25,10 +25,14 @@ public class DialogueManager : MonoBehaviour
     public InventoryItem my_hasItemNode1;
     public InventoryItem my_itemToAddNode2;
     public InventoryItem my_hasItemNode2;
+    public InventoryItem my_losesNode1;
+    public InventoryItem my_losesNode2;
     
     public Inventory my_targetInventory;
 
     public GameObject eventTracker;
+
+    private int i = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -54,7 +58,10 @@ public class DialogueManager : MonoBehaviour
         my_hasItemNode1 = dialogueDisplay.GetComponent<DialogueDisplay>().hasItemNode1;
         my_itemToAddNode2 = dialogueDisplay.GetComponent<DialogueDisplay>().itemToReceiveNode2;
         my_hasItemNode2 = dialogueDisplay.GetComponent<DialogueDisplay>().hasItemNode2;
-        
+        my_losesNode1 = dialogueDisplay.GetComponent<DialogueDisplay>().PlayerLosesNode1;
+        my_losesNode2 = dialogueDisplay.GetComponent<DialogueDisplay>().PlayerLosesNode2;
+
+
 
     }
 
@@ -62,10 +69,10 @@ public class DialogueManager : MonoBehaviour
     {
         // button click should change the active node to the next node... 
         if (responseNodeOne != null){
-            //this is always true despite the player not having the item???
                 
             if (my_hasItemNode1 == null || eventTracker.GetComponent<MyEventTracker>().my_CheckInventory(my_hasItemNode1.name))
             {
+                my_LoseItem(my_losesNode1);
                 my_AddItem(my_itemToAddNode1);
                 active = responseNodeOne; /// this isn't actually changing the active?
                 dialogueDisplay.GetComponent<DialogueDisplay>().dialogue = active;
@@ -87,6 +94,7 @@ public class DialogueManager : MonoBehaviour
            if (my_hasItemNode2 == null || eventTracker.GetComponent<MyEventTracker>().my_CheckInventory(my_hasItemNode2.name))
 
            {
+               my_LoseItem(my_losesNode2);
                my_AddItem(my_itemToAddNode2);
                active = responseNodeTwo;
                dialogueDisplay.GetComponent<DialogueDisplay>().dialogue = active;
@@ -118,6 +126,23 @@ public class DialogueManager : MonoBehaviour
     {
       //  my_itemToAdd = dialogueDisplay.GetComponent<DialogueDisplay>().itemToReceive;
         my_targetInventory.AddItem(my_itemToAdd, 1);
+    }
+
+    public void my_LoseItem(InventoryItem my_lostItem)
+    {
+        while (true)
+        {
+            if (my_targetInventory.Content[i].ItemName == my_lostItem.ItemName)
+            {
+                my_targetInventory.RemoveItem(i, 1); 
+                break;
+            }
+
+            i++;
+            Debug.Log(i);
+
+        }
+
     }
     
 }
