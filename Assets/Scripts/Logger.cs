d92fac1c8a30d306f3ece6c541d06dfa3b486ea1
Peter Drake <drake@lclark.edu>
2019-07-07 15:22:07 -0700

    Logger logs time since startup and message. TransparentTable modified to avoid redundant events.

---
+++ Assets/Scripts/Logger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+
+public static class Logger
+{
+    // Start is called before the first frame update
+    public static void Log(string message)
+    {
+        int time = (int)Math.Round(Time.realtimeSinceStartup);
+        Debug.Log(time + ": " + message);
+    }
+    
+}
