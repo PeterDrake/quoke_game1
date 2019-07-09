b4acf82f7af527d1d2deade882531e6a85a86b86
Peter Huizenga <peterhuizenga@lclark.edu>
2019-07-09 16:30:52 -0700

    Level one updates

---
+++ Assets/FallingFurnitureDeath.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FallingFurnitureDeath : MonoBehaviour
+{
+    
+    public Slider health;
+    public GameObject DeathScreen;
+
+    public Text death;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player Hit");
+            death.text = "Crushed by falling object";
+            health.GetComponent<Slider>().value = health.GetComponent<Slider>().minValue;
+
+        }
+    }
+
+}
---
+++ Assets/QuakeFurniture.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuakeFurniture : MonoBehaviour
+{
+    public GameObject[] falling_objects;
+
+    private int i = 0;
+
+
+    public void Drop()
+    {
+        StartCoroutine(DropEm());
+    }
+
+    public IEnumerator DropEm()
+    {
+        while (i < falling_objects.Length)
+        {
+            falling_objects[i].SetActive(true);
+            yield return new WaitForSeconds(.5f);
+            i++;
+        }
+    }
+}
