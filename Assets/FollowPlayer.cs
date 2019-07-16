bbbb404d5a75386cb549758062c112219d00fb47
Liza Clairagneau <lclairagneau@lclark.edu>
2019-07-16 15:09:46 -0700

    Following NPC

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -5,6 +5,7 @@ using System.Security.Cryptography.X509Certificates;
 using UnityEngine.AI;
 using MoreMountains.TopDownEngine;
 using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
 
 
 public class FollowPlayer : MonoBehaviour
@@ -16,55 +17,48 @@ public class FollowPlayer : MonoBehaviour
     private GameObject player;
    // public NavMeshAgent npc;
     public int i = -500;
-
+    public Rigidbody rigidbody;
     private Vector3 pos;
-
     private Vector3 diff;
-    
-    
-    public float speed = .0001f;
+    private Vector3 near;
+
+
+    public float speed;
    // private float speed = 3.0f;
     void Start ()
     {
+        rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
         player = GameObject.FindWithTag("Player");
-        me = GameObject.FindWithTag("NPC2");
+        //me = GameObject.FindWithTag("NPC2");
+        diff = transform.position - player.transform.position;
+        diff.y = 1;
         //At the start of the game, the zombies will find the gameobject called wayPoint.
-      //  wayPoint = GameObject.Find("wayPoint");
-//      Vector3 add = new Vector3(2, 0, 4);
-//      var position = this.transform.position;
-//      pos = position + (player.transform.position - position); 
+
     }
  
-    void Update ()
+    void LateUpdate ()
     {
-        //        //wayPointPos = new Vector3(wayPoint.transform.position.x, transform.position.y, wayPoint.transform.position.z);
-//        //Here, the zombie's will follow the waypoint.
-//        
-//        
-//        if (i>0)
-//        {       
-//            npc.SetDestination(pos);
-//            Debug.Log(npc.destination);
-//            var position = this.transform.position;
-//            diff = player.transform.position - position;
-//            pos = position + diff;
-//
-//        }
-//        
-//        i
-
-        if (i>0)
-        { 
-            float interpolation = speed * Time.deltaTime;
-          //  Debug.Log(interpolation);
-            Vector3 position = me.transform.position;
-            position.z = Mathf.Lerp(me.transform.position.z, player.transform.position.z, interpolation);
-            position.x = Mathf.Lerp(me.transform.position.x, player.transform.position.x, interpolation);
-            GameObject.FindWithTag("NPC2").transform.position = position;
-            transform.position = position;
-            Debug.Log("player position:"+player.transform.position);
-        }
-        i++;
-
+//            float interpolation = speed * Time.deltaTime;
+//            Vector3 position = me.transform.position;
+//            Debug.Log(position);
+//            position.z = Mathf.Lerp(me.transform.position.z, player.transform.position.z, interpolation);
+//            position.x = Mathf.Lerp(me.transform.position.x, player.transform.position.x, interpolation);
+//            Debug.Log(position);
+//            GameObject.FindWithTag("NPC2").transform.position = position;
+//            transform.Translate(position); 
+//            Debug.Log("player position:"+player.transform.position)
+        
+            Debug.Log(transform.position);
+            
+            rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+            pos = transform.position;
+            pos.y = 1;
+            near = player.transform.position - new Vector3(1, 0, 1);
+            transform.position = Vector3.MoveTowards(pos, near, speed * Time.deltaTime);
+           //transform.position = player.transform.position + diff;
+           rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+           
+            Debug.Log(transform.position);
+        
     }
 }
