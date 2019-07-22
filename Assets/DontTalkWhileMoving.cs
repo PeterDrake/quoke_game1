7d114b76365605f65682fc341b92d68422e5b28b
Peter Huizenga <peterhuizenga@lclark.edu>
2019-07-22 12:00:23 -0700

    messing with AI nav level 3

---
+++ Assets/DontTalkWhileMoving.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontTalkWhileMoving : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ImMoving());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator ImMoving()
+    {
+        GetComponent<CapsuleCollider>().enabled = false;
+        yield return new WaitForSeconds(10f);
+        GetComponent<CapsuleCollider>().enabled = true;
+
+    }
+}
