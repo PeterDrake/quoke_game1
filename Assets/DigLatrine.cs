893e035ece94014e73f791392518fd9a7c8d8d88
Peter Huizenga <peterhuizenga@lclark.edu>
2019-07-23 16:04:21 -0700

    prototyping L3 tasks

---
+++ Assets/DigLatrine.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DigLatrine : MonoBehaviour
+{
+    
+    private bool isColliding;
+
+    public GameObject eventTracker;
+   
+    private void OnTriggerStay(Collider other) 
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown("e"))
+            {
+                if (isColliding) return;
+                isColliding = true;
+
+                if (eventTracker.GetComponent<MyEventTracker>().my_CheckInventory("shovel"))
+                {
+                    Debug.Log("success");
+                }
+                else
+                {
+                    Debug.Log("you dont have a shovel");
+                }
+
+               
+
+            }
+            
+
+        }
+    }
+}
---
+++ Assets/Scripts/TapHeater.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapHeater : MonoBehaviour
+{
+    private bool isColliding;
+
+    public GameObject eventTracker;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other) 
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown("e"))
+            {
+                if (isColliding) return;
+                isColliding = true;
+
+                if (eventTracker.GetComponent<MyEventTracker>().my_CheckInventory("hose"))
+                {
+                    Debug.Log("success");
+                }
+                else
+                {
+                    Debug.Log("you dont have a hose");
+                }
+
+               
+
+            }
+            
+
+        }
+    }
+}
