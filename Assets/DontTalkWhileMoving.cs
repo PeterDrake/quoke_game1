ee4e18243e91e36de0f6ce3f62753649da401183
Peter Huizenga <peterhuizenga@lclark.edu>
2019-07-23 10:47:35 -0700

    added house to L3

--- Assets/DontTalkWhileMoving.cs
+++ Assets/DontTalkWhileMoving.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using MoreMountains.InventoryEngine;
 using UnityEngine;
 using UnityStandardAssets.Characters.ThirdPerson;
 
@@ -8,6 +9,8 @@ public class DontTalkWhileMoving : MonoBehaviour
     public GameObject ThisCharacter;
 
     public GameObject AItarget;
+
+    public GameObject dialogueManager;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +27,8 @@ public class DontTalkWhileMoving : MonoBehaviour
     {
         StartCoroutine(ImMoving());
         ThisCharacter.GetComponent<AICharacterControl>().target = AItarget.transform;
+        dialogueManager.GetComponent<DialogueManager>().NPCL3 = false;
+
 
 
     }
@@ -32,8 +37,9 @@ public class DontTalkWhileMoving : MonoBehaviour
     public IEnumerator ImMoving()
     {
         GetComponent<CapsuleCollider>().enabled = false;
-        yield return new WaitForSeconds(14f);
+        yield return new WaitForSeconds(17f);
         GetComponent<CapsuleCollider>().enabled = true;
+        
 
     }
 }
