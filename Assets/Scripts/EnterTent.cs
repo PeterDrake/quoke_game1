1ccfa8a3fc1d0822d7f7e1c6c6cbe95ddcaaffd6
Peter Huizenga <peterhuizenga@lclark.edu>
2019-07-25 16:40:37 -0700

    end of day

---
+++ Assets/Scripts/EnterTent.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterTent : MonoBehaviour
+{
+    public GameObject eventTracker;
+    private bool isColliding;
+    // Start is called before the first frame update
+    private void OnTriggerStay(Collider other) 
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown("e"))
+            {
+                if (isColliding) return;
+                isColliding = true;
+
+                if (eventTracker.GetComponent<MyEventTracker>().my_CheckInventory("Handgun"))
+                {
+                    Debug.Log("you cant come in with a gun");
+
+            
+                }
+                else
+                {
+                    Debug.Log("you can enter");
+                }
+
+               
+
+            }
+            
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isColliding = false;
+        }
+    }
+    
+    
+}
