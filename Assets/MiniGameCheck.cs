5f54f8c8c27ee414e211de12f46daa7545863f94
Peter Huizenga <peterhuizenga@lclark.edu>
2019-09-16 16:36:26 -0700

    minigame prototype

---
+++ Assets/MiniGameCheck.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGameCheck : MonoBehaviour
+{
+
+    public string correctTag;
+
+    private MiniGameMaster MasterCheck;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(correctTag))
+        {
+            //Debug.Log(correctTag +" is correct");
+            MasterCheck = GameObject.Find("MinigameMaster").GetComponent<MiniGameMaster>();
+            if (correctTag == "Blue")
+            {
+                MasterCheck.Blue = true;
+            }
+            if (correctTag == "Orange")
+            {
+                MasterCheck.Orange = true;
+            }
+            if (correctTag == "Green")
+            {
+                MasterCheck.Green = true;
+            }
+        }
+        else
+        {
+            //Debug.Log("An item is in the wrong place"); 
+        }
+    }
+    
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(correctTag))
+        {
+            //Debug.Log(correctTag +" is correct");
+            MasterCheck = GameObject.Find("MinigameMaster").GetComponent<MiniGameMaster>();
+            if (correctTag == "Blue")
+            {
+                MasterCheck.Blue = false;
+            }
+            if (correctTag == "Orange")
+            {
+                MasterCheck.Orange = false;
+            }
+            if (correctTag == "Green")
+            {
+                MasterCheck.Green = false;
+            }
+        }
+        else
+        {
+            //Debug.Log("An item is in the wrong place"); 
+        }
+    } 
+    
+}
---
+++ Assets/MiniGameMaster.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGameMaster : MonoBehaviour
+{
+    public bool Orange;
+
+    public bool Green;
+
+    public bool Blue; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CheckCorrect()
+    {
+        if (Orange && Green && Blue)
+        {
+            Debug.Log("You solved the puzzle");
+        }
+        else
+        {
+            Debug.Log("Something isn't in the right place");
+        }
+    }
+    
+}
