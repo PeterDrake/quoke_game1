dca2b7ea01fcf653158f4ac2b3b171bfada26c3c
Gabe Mertz <gabe.mertz@crms.org>
2019-09-17 15:07:14 -0700

    began work on gas valve minigame

---
+++ Assets/Scripts/WrenchMiniGame/ObjectFollowMouse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFollowMouse : MonoBehaviour
+{
+	//on update, follow the mouse
+	public void Update()
+	{
+
+	}
+}
---
+++ Assets/Scripts/WrenchMiniGame/StartMiniGame.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMiniGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public GameObject wrenchPrefab;
+	public GameObject flange; //flange should be a gameobject with a box colider set to trigger and the 
+
+	public Vector3 attachedWrenchPosition;
+
+	GameObject wrench;
+
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void AttachWrench()
+	{
+		Destroy(wrench.GetComponent<ObjectFollowMouse>());
+		flange.GetComponent<CollisionCallback>().RemoveCallback(wrench.tag);
+		wrench.transform.position = attachedWrenchPosition;
+	}
+
+	public void StartGame()
+	{
+		if (wrench) return;
+		wrench = CreateWrench();
+		flange.GetComponent<CollisionCallback>().AddCallback(wrench.tag, AttachWrench);
+		
+	}
+
+	GameObject CreateWrench()
+	{
+		var newWrench =  Instantiate(wrenchPrefab, Input.mousePosition, Quaternion.identity);
+		newWrench.AddComponent<ObjectFollowMouse>();
+		return newWrench;
+	}
+}
