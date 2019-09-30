e6f0ab067826693378a0d249ba150dc9bfa2471f
Peter Huizenga <peterhuizenga@lclark.edu>
2019-09-30 15:48:02 -0700

    semi functional character

---
+++ Assets/StartupFollowCam.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class StartupFollowCam : MonoBehaviour
+{
+    public CinemachineVirtualCamera cmCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FollowPlayer());
+    }
+
+    public IEnumerator FollowPlayer()
+    {
+        yield return new WaitForEndOfFrame();
+        GameObject myPlayer = GameObject.FindWithTag("Player");
+        cmCam.Follow = myPlayer.transform;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    
+}
