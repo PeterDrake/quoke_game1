dc28c5ecc37cc98a186a38e2539ab33ca1406ff2
Liam Beveridge <liamrbeveridge@gmail.com>
2019-10-24 16:30:14 -0700

    adding liams scene with fixed interactables

---
+++ Assets/ToiletInteraction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToiletInteraction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Interaction()
+    {
+        gameObject.GetComponent<InteractWithObject>().SetInteractText("Press 'E' to Use Toilet");
+        //gameObject.GetComponent<InteractWithObject>().DeleteItems();
+        StatusManager.Manager.AffectRelief(50);
+    }
+}
