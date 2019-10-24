d105ff1437928364a7d7b23cfa75b88691746f67
Gabe Mertz <gabe.mertz@crms.org>
2019-10-24 15:18:48 -0700

    bookcase script and various scene changes

---
+++ Assets/Bookcase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bookcase : MonoBehaviour
+{
+    private int state = 0;
+    /*
+     * for function called by InteractWithObject
+     * on first pass, check if player has tools, if so, display "Press 'e' to secure bookcase"
+     * if not, display text "This bookcase could fall over in an earthquake, I should secure it"
+     * on second pass "MESSAGE 2"
+     * on third pass "Press 'e' to stabilize the bookcase". * On exit, trigger earthquake *
+     */
+    
+    
+    
+}
