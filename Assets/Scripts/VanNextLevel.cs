2c5c71db62724ba543e18d1aa791c745e14ab690
Gabe Mertz <gabe.mertz@crms.org>
2019-11-03 14:56:04 -0800

    Van doesnt blink if player cant interact with it. Interactables will not display anything if their interaction text is set to empty string("")

--- Assets/Scripts/VanNextLevel.cs
+++ Assets/Scripts/VanNextLevel.cs
@@ -6,23 +6,24 @@ using UnityEngine.SceneManagement;
 
 public class VanNextLevel : MonoBehaviour
 {
-    private const string SATISFIED = "Press 'e' to take a rest in Adam's van.";
+    private const string SATISFIED = "Press 'e' to rest in Adam's van";
     private const string NOT_SATISFIED = "";
     
     private InteractWithObject _interact;
-    private bool satisfied = false;
+    private bool _satisfied = false;
     private void Start()
     {
-        ObjectiveManager.Instance.Register("BOOKCASE",() => satisfied = true);
+        ObjectiveManager.Instance.Register("BOOKCASE",() => _satisfied = true);
         _interact = GetComponent<InteractWithObject>();
     }
     
     public void OnEnter()
     {
-        _interact.SetInteractText(satisfied? SATISFIED:NOT_SATISFIED);
+        _interact.SetInteractText(_satisfied? SATISFIED:NOT_SATISFIED);
+        _interact.BlinkWhenPlayerNear = _satisfied;
     }
     public void Interaction()
     {
-        if (satisfied) SceneManager.LoadScene("DemoMenu", LoadSceneMode.Single);
+        if (_satisfied) SceneManager.LoadScene("DemoMenu", LoadSceneMode.Single);
     }
 }
