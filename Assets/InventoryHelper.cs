a8b9d85f671fb57999f66df9d13224f3170875ae
Gabe Mertz <gabe.mertz@crms.org>
2019-11-08 12:33:42 -0800

    InventoryHelper script helps with checking items, and can call functions every time an item is added (to check for conditions). Changed the condition chekcing of SanitationBuilt, so that the condition is satisfied independantly of the player interacting with the collider

---
+++ Assets/InventoryHelper.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MoreMountains.InventoryEngine;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InventoryHelper : MonoBehaviour
+{
+    public UnityEvent CheckOnAdd;
+    // Script to help with adding, checking, removing items
+    public Inventory _inventory;
+
+    public void AddItem(BaseItem item, int amt)
+    {
+        CheckOnAdd.Invoke();
+        _inventory.AddItem(item, amt);
+    }
+
+    public bool HasItem(BaseItem item, int amt)
+    {
+        return (_inventory.InventoryContains(item.name).Count >= amt);
+    }
+
+    public bool HasItem(BaseItem[] items, int[] amts)
+    {
+        for (int i = 0; i < items.Length; i++)
+            if (!HasItem(items[i], amts[i]))
+                return false;
+        return true;
+    }
+
+
+}
--- Assets/Scripts/SanitationBuilt.cs
+++ Assets/Scripts/SanitationBuilt.cs
@@ -10,7 +10,7 @@ public class SanitationBuilt : MonoBehaviour
 {
      
     private InteractWithObject _interact;
-    private Inventory _inventory;
+    private InventoryHelper _inventory;
  
     private BaseItem Bucket;
     private BaseItem Bag;
@@ -21,22 +21,27 @@ public class SanitationBuilt : MonoBehaviour
     private bool HasBuckets;
     private bool HasBag;
     private bool HasSawdust;
+
+    private byte Conditions;
+    
      
     void Start()
     {
         _interact = GetComponent<InteractWithObject>();
-        _inventory = GameObject.FindWithTag("MainInventory").GetComponent<Inventory>();
+        _inventory = GameObject.FindWithTag("MainInventory").GetComponent<InventoryHelper>();
          
         Bucket =  Resources.Load<BaseItem>("Items/Bucket");
         Bag =  Resources.Load<BaseItem>("Items/Bag");
         Sawdust =  Resources.Load<BaseItem>("Items/Sawdust");
         Sanitizer =  Resources.Load<BaseItem>("Items/Sanitizer");
+        _inventory.CheckOnAdd.AddListener(UpdateConditions);
+        
+        
     }
- 
+    
     public void Interaction()
     {
-        if (_inventory.InventoryContains(Bucket.name).Count > 1 && _inventory.InventoryContains(Bag.name).Count > 0 && 
-            _inventory.InventoryContains(Sawdust.name).Count > 0 && _inventory.InventoryContains(Sanitizer.name).Count > 0)
+        if ((Conditions^0xF) == 0)
         {
             Debug.Log("Toilet Time!");
             SceneManager.LoadScene("MiniGame", LoadSceneMode.Single);
@@ -45,5 +50,22 @@ public class SanitationBuilt : MonoBehaviour
         {
             _interact.SetInteractText("You need to gather more items");
         }
+    }
+
+    private void UpdateConditions() //called every time an item is added to the inventory 
+    {
+        if ((Conditions ^ 0xF) == 0) return;
+        
+        if ((Conditions & 0x1) == 0 && _inventory.HasItem(Bucket, 2)) //first condition not met
+            Conditions &= 0x1;
+        
+        if ((Conditions & 0x2) == 0 && _inventory.HasItem(Bag, 2)) //second condition not met
+            Conditions &= 0x1;
+        
+        if ((Conditions & 0x8) == 0 && _inventory.HasItem(Sawdust, 1)) //third condition not met
+            Conditions &= 0x1;
+        
+        if ((Conditions & 0x1) == 0 && _inventory.HasItem(Sanitizer, 1)) //fourth condition not met
+            Conditions &= 0x1;
     }
 }
