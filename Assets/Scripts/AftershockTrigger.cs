7971e5630159bc3f13f115f4e4d48e5deb06c4cc
Gabe Mertz <gabe.mertz@crms.org>
2019-11-17 18:14:49 -0800

    added logging for table and house entrace/exit and removed collider from rug in bedroom

--- Assets/Scripts/AftershockTrigger.cs
+++ Assets/Scripts/AftershockTrigger.cs
@@ -6,9 +6,13 @@ using UnityEngine;
 
 public class AftershockTrigger : MonoBehaviour
 {
+
+    public InformationCanvas _canvas;
     private void OnTriggerEnter(Collider other)
     {
         QuakeManager.Instance.TriggerCountdown(2f);
+        _canvas.DisplayInfo("Have a look around...");
+        Logger.Instance.Log("Player has left the house");
         Destroy(gameObject);
     }
 }
