75a7dd7306dd152c50a5e28ced77e36906c4465c
Anna <adesmet@lclark.edu>
2020-03-17 14:41:18 -0700

    Initial commit

--- Assets/Scripts/Menu/Exit.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class Exit : MonoBehaviour
-{
-    public void ExitGame()
-    {
-        Application.Quit();
-    }
-}
--- Assets/Scripts/Non-Specific/Inventory/Items/Drinkable.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Drinkable Item", menuName = "Items/Drinkable")]
-public class Drinkable : Item
-{
-    public int hydrationChange = 0;
-    public bool killPlayer;
-    public string DeathMessage;
-}
