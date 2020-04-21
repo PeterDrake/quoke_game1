cb1ad16aa8366fd4bb86c8c157bb7cfdab9e84a8
Lanaparezanin <Lanaparezanin>
2020-04-21 14:13:49 -0700

    Minigame

---
+++ Assets/Scripts/ToiletMinigame/Pamphlet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Pamphlet : MonoBehaviour
+{
+    public GameObject pamphlet;
+    public Text buttonText;
+
+    private string openText = "Open Pamphlet";
+    private string closeText = "Close Pamphlet";
+
+    private bool open;
+
+    private void Awake()
+    {
+        pamphlet.SetActive(false);
+        buttonText.text = openText;
+    }
+
+    public void toggle()
+    {
+        if (open)
+        {
+            pamphlet.SetActive(false);
+            buttonText.text = openText;
+        }
+        else
+        {
+            pamphlet.SetActive(true);
+            buttonText.text = closeText;
+        }
+        open = !open;
+    }
+
+}
+
