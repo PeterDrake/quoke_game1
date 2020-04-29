fa3cdd90d1657bf54af2e6288d99d62daa9b0a68
B <blytheballesteros@lclark.edu>
2020-04-29 01:22:41 -1000

    open pamphlet button in inventory
    doesnt work tho

---
+++ Assets/Scripts/Non-Specific/Inventory/Actions/OpenMe.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Open Action", menuName = "Items/Actions/Open")]
+public class OpenMe : ItemAction
+{
+
+    public override bool Use(ref Item i)
+    { 
+        return true;
+    }
+}
